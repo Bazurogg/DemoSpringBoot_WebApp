248a35ac5d9b4b78430a40d1313e018e2e5e7230
Bazurogg <bazurogg@gmail.com>
2025-02-26 16:44:23 +0100

    jenkinsfile build Maven

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,5 +17,10 @@ pipeline {
                 }
             }
         }
+        stage('Build Maven') {
+            steps {
+                bat 'mvn clean package'
+            }
+        }
     }
 }
