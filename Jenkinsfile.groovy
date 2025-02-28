0857bf4a468f449864d96628d8066457ae1b32f7
Bazurogg <bazurogg@gmail.com>
2025-02-28 08:51:54 +0100

    update docker compose & jenkins files for build on Jenkins

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,13 +10,16 @@ pipeline {
         registryCredential = 'DockerHubAccount'
         dockerImage = ''
     }
+
     stages {
+
         stage('Clean workspace') {
             steps {
                 cleanWs()
             }
         }
-        stage('Git Checkout') {
+
+        stage('Git Checkout - WebApp') {
             steps {
                 script {
                     git branch: 'master',
@@ -25,13 +28,24 @@ pipeline {
                 }
             }
         }
-        stage('Build Maven') {
+
+        stage('Git Checkout - API') {
+            steps {
+                script {
+                    git branch: 'master',
+                            credentialsId: 'Github Classic Token',
+                            url: 'https://github.com/Bazurogg/DemoSpringBoot_API'
+                }
+            }
+        }
+
+        stage('Build Maven - WebApp') {
             steps {
                 bat 'mvn clean package'
             }
         }
 
-        stage('Build Docker Image') {
+        stage('Build Docker Image - WebApp') {
             steps {
                 script {
                     docker.build('bazurogg/webspringboot2025:latest', '-f Dockerfile .')
@@ -39,8 +53,16 @@ pipeline {
             }
         }
 
+        stage('Build Docker Image - API') {
+            steps {
+                script {
+                    docker.build('bazurogg/apispringboot2025:latest', '-f Dockerfile .')
+                }
+            }
+        }
+
         // push de l'image dans le dockerHub
-        stage('Push to Docker Hub') {
+        stage('Push to Docker Hub - WebApp') {
             steps {
                 script {
                     // interaction avec un registre Docker
@@ -49,6 +71,16 @@ pipeline {
                      docker.withRegistry('', registryCredential) {
                          //".push()" Envoie de l'image Docker vers le registre spécifié
                         docker.image('bazurogg/webspringboot2025:latest').push()
+                    }
+                }
+            }
+        }
+
+        stage('Push to Docker Hub - API') {
+            steps {
+                script {
+                    docker.withRegistry('', registryCredential) {
+                        docker.image('bazurogg/springbootapi2025:latest').push()
                     }
                 }
             }
@@ -64,5 +96,7 @@ pipeline {
                 }
             }
         }
+
     }
+
 }
